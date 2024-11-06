90233f5c8a2e33b35bf5ff954d47c8f8afa3a319
Farahbany24 <farahrabbany19@gmail.com>
2024-11-06 14:03:22 +0700

    form & data

--- form.php
+++ form.php
@@ -3,35 +3,91 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form</title>
-</head>
-<body>
-    <form action="data.php" enctype="multipart/form-data" method="POST" >
-        <h2>Input Data Product</h2>
-
-        <!-- input nama : single line text input  -->
-        Name : <input type="text" name="name" value="" ><br/>
-        Description : <input type="text" name="description" id=""><br/>
-        Price : <input type="number" name="prie" id=""><br/>
-        Brand_id : <input type="text" name="brand_id" id=""> <br/>
-        Tanggal Lahir : <input type="date" name="tgl_lahir" id=""><br/>
-        Alamat : <textarea name="alamat" id="" cols="30" rows="10"></textarea>
-        <br/>
-        No Telp : <input type="number" name="no_telp" id=""><br/>
-        Gender : <input type="radio" name="gender" id="" value=""> Laki 
-                 <input type="radio" name="gender" id="" value=""> Perempuan
-                 <br/>
-        Hobi : <input type="checkbox" name="hobi1" id="" value=""> Membaca <br/>
-        <input type="checkbox" name="hobi2" id="" value=""> berlari <br/>
-        <input type="checkbox" name="hobi3" id="" value=""> tidur <br/>
-        <input type="checkbox" name="hobi4" id="" value=""> main game <br/>
-        Foto : <input type="file" name="foto" id=""><br/>
-
-        <input type="submit" value="Input Data" name="inputData"> 
-        <!-- <button type="submit">Inputkan</button> -->
-
-
-    </form>
+    <title>Input Data Product</title>
     
+</head>
+
+<body>
+
+    <form action="data.php" enctype="multipart/form-data" method="POST" >
+        <!-- input nama : single line text input  -->
+        <center>
+            <h4>Input Data Product</h4>
+                <table>
+                    <tr>
+                        <td>
+                            Name
+                        </td>
+                        <td>
+                            <input type="text" placeholder="Name" name="name" value="" ></br>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            Description
+                        </td>
+                        <td>
+                            <textarea type="text" placeholder="Description" name="description" id="" cols="30" rows="10"></textarea><br/>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            Price
+                        </td>
+                        <td>
+                            <input type="number" placeholder="Price" name="price" value="" ><br/>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            Brand_id
+                        </td>
+                        <td>
+                            <select name="Brand_id" id="">
+                                <option value="0">--Pilih Brand--</option>
+                                <option value="1">Converse</option>
+                                <option value="2">Nike</option>
+                                <option value="3">New Balance</option>
+                                <option value="4">Onitsuka Tiger</option>
+                                <option value="5">Adidas</option>
+                                <option value="6">Polo Ralph</option>
+                            </select> <br/>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            Category_id
+                        </td>
+                        <td>
+                            <select name="category_id" id="">
+                                <option value="0">--Pilih Category--</option>
+                                <option value="1">Men</option>
+                                <option value="2">Women</option>
+                                <option value="3">Kids</option>
+                            </select> <br/>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            Image
+                        </td>
+                        <td>
+                        <input type="file" name="image" placeholder="Image" id=""><br/>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                        <button>Submit</button>
+                        </td>
+                    </tr>
+                </table>
+        </center>
+    </form>
 </body>
 </html>
